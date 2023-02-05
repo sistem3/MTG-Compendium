8052342493cc80bb79bb579d0ee8d46903443273
Ben Burton <sistematic3@yahoo.co.uk>
2023-02-05 18:50:15 +0000

    Mana Icons Mapping - Adding Mixed Colour Mana Icon Mapping + Tidy up of mana cost pipe switch case

--- src/app/card-library/pipes/mana-cost.pipe.spec.ts
+++ src/app/card-library/pipes/mana-cost.pipe.spec.ts
@@ -8,6 +8,7 @@ describe('ManaCostPipe', () => {
 
   it('return a formatted string based on input value', () => {
     const pipe = new ManaCostPipe();
+    // Colour Checks
     const whiteTest: any = pipe.transform('{W}', 'cost');
     expect(whiteTest.includes('white')).toBe(true);
     const blueTest: any = pipe.transform('{U}', 'cost');
@@ -18,12 +19,36 @@ describe('ManaCostPipe', () => {
     expect(greenTest.includes('green')).toBe(true);
     const blackTest: any = pipe.transform('{B}', 'cost');
     expect(blackTest.includes('black')).toBe(true);
+    // Default Check
     const defaultTest: any = pipe.transform('{1}', 'cost');
     expect(defaultTest.includes('icon-1')).toBe(true);
     expect(defaultTest.includes('mana-icon-holder small')).toBe(true);
+    // Inline Check
     const inlineTest: any = pipe.transform('{W}', 'inline');
     expect(inlineTest.includes('white')).toBe(true);
     expect(inlineTest.includes('mana-icon-holder extra-small')).toBe(true);
+    // Mixed Colours Check
+    const whiteBlueTest: any = pipe.transform('{W/U}', 'cost');
+    expect(whiteBlueTest.includes('white-blue')).toBe(true);
+    const whiteBlackTest: any = pipe.transform('{W/B}', 'cost');
+    expect(whiteBlackTest.includes('white-black')).toBe(true);
+    const blueBlackTest: any = pipe.transform('{U/B}', 'cost');
+    expect(blueBlackTest.includes('blue-black')).toBe(true);
+    const blueRedTest: any = pipe.transform('{U/R}', 'cost');
+    expect(blueRedTest.includes('blue-red')).toBe(true);
+    const blackRedTest: any = pipe.transform('{B/R}', 'cost');
+    expect(blackRedTest.includes('black-red')).toBe(true);
+    const blackGreenTest: any = pipe.transform('{B/G}', 'cost');
+    expect(blackGreenTest.includes('black-green')).toBe(true);
+    const redWhiteTest: any = pipe.transform('{R/W}', 'cost');
+    expect(redWhiteTest.includes('red-white')).toBe(true);
+    const redGreenTest: any = pipe.transform('{R/G}', 'cost');
+    expect(redGreenTest.includes('red-green')).toBe(true);
+    const greenWhiteTest: any = pipe.transform('{G/W}', 'cost');
+    expect(greenWhiteTest.includes('green-white')).toBe(true);
+    const greenBlueTest: any = pipe.transform('{G/B}', 'cost');
+    expect(greenBlueTest.includes('green-blue')).toBe(true);
+    // Empty Check
     const emptyTest: any = pipe.transform('', 'inline');
     expect(emptyTest).toBe('');
   });
--- src/app/card-library/pipes/mana-cost.pipe.ts
+++ src/app/card-library/pipes/mana-cost.pipe.ts
@@ -30,32 +30,58 @@ export class ManaCostPipe implements PipeTransform {
   }
 
   getManaTemplate(item: string, spanOpener: string, returnedString: string) {
+    let iconTemplate;
     switch (item) {
       case 'W':
-        returnedString = returnedString + spanOpener +
-          '<span class="mana-icon white"></span></span>';
+        iconTemplate = '<span class="mana-icon white"></span></span>';
         break;
       case 'U':
-        returnedString = returnedString + spanOpener +
-          '<span class="mana-icon blue"></span></span>';
+        iconTemplate = '<span class="mana-icon blue"></span></span>';
         break;
       case 'R':
-        returnedString = returnedString + spanOpener +
-          '<span class="mana-icon red"></span></span>';
+        iconTemplate = '<span class="mana-icon red"></span></span>';
         break;
       case 'G':
-        returnedString = returnedString + spanOpener +
-          '<span class="mana-icon green"></span></span>';
+        iconTemplate = '<span class="mana-icon green"></span></span>';
         break;
       case 'B':
-        returnedString = returnedString + spanOpener +
-          '<span class="mana-icon black"></span></span>';
+        iconTemplate = '<span class="mana-icon black"></span></span>';
+        break;
+      case 'W/U':
+        iconTemplate = '<span class="mana-icon white-blue"></span></span>';
+        break;
+      case 'W/B':
+        iconTemplate = '<span class="mana-icon white-black"></span></span>';
+        break;
+      case 'U/B':
+        iconTemplate = '<span class="mana-icon blue-black"></span></span>';
+        break;
+      case 'U/R':
+        iconTemplate = '<span class="mana-icon blue-red"></span></span>';
+        break;
+      case 'B/R':
+        iconTemplate = '<span class="mana-icon black-red"></span></span>';
+        break;
+      case 'B/G':
+        iconTemplate = '<span class="mana-icon black-green"></span></span>';
+        break;
+      case 'R/W':
+        iconTemplate = '<span class="mana-icon red-white"></span></span>';
+        break;
+      case 'R/G':
+        iconTemplate = '<span class="mana-icon red-green"></span></span>';
+        break;
+      case 'G/W':
+        iconTemplate = '<span class="mana-icon green-white"></span></span>';
+        break;
+      case 'G/B':
+        iconTemplate = '<span class="mana-icon green-blue"></span></span>';
         break;
       default:
-        returnedString = returnedString + spanOpener +
-          '<span class="mana-icon icon-' + item + '"></span></span>';
+        iconTemplate = '<span class="mana-icon icon-' + item + '"></span></span>';
         break;
     }
+    returnedString = returnedString + spanOpener + iconTemplate;
     return returnedString;
   }
 
